8390b9b02473bb8bbdb32dfbb59a80c5eae16231
WilliamPrakash <willprakash@gmail.com>
2021-02-01 13:26:03 -0500

    New folder for interview questions

---
+++ Interview-Questions/testing.js
@@ -0,0 +1,11 @@
+
+var x = 9;
+//sconsole.log(isNaN x)
+
+// This is shorthand for b = 3; and a = b; Therefore, a is undefined and b is defined
+/*(function(){
+    var a = b = 3;
+  })();
+  
+  console.log("a defined? " + (typeof a !== 'undefined'));
+  console.log("b defined? " + (typeof b !== 'undefined'));  */
