818eb7a43d8a228e81a4acf7e9016f79dd76033d
WilliamPrakash <willprakash@gmail.com>
2022-07-11 12:17:25 -0400

    Starting a HashTable + Linked List

--- Data_Structures/HashTable.js
+++ Data_Structures/HashTable.js
@@ -1,28 +1,20 @@
 
 class HashTable {
   constructor() {
-    // All key/value pairs will be stored inside the table property
-    // Since the HashTable class only has 127 buckets,
-    // that means the _hash() must return a num between 0 and 127
     this.table = new Array(127)
     this.size = 0
   }
 
-  // Generates index -> called by set()
   _hash(key) {
     let hash = 0
     for(let i=0; i < key.length; i++) {
-      // simple way to create hash (sum the ASCII code characters)
       hash += key.charCodeAt(i)
     }
     console.log('Key: ' + key + ' created hash: ' + hash)
-    // this ensures the hash value doesn't exceed the bucket size
-    // mod return the remainder of two integers that have been divided
     return hash % this.table.length
   }
 
   set(key, value) {
-    // using hash function to generate the index
     const index = this._hash(key)
     this.table[index] = [key,value]
     this.size++
