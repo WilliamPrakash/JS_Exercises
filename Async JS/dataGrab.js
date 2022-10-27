81cd0763a05d1e4ab0a356280f3b019b82ef8b16
WilliamPrakash <willprakash@gmail.com>
2022-10-26 22:02:41 -0400

    trying to make a module folder

---
+++ Async JS/dataGrab.js
@@ -0,0 +1,16 @@
+
+export function grab() {
+    const request = new XMLHttpRequest()
+
+    // we can track the progress of a request using an event listener
+    // this will fire every time there is a state change in the request
+     request.addEventListener('readystatechange', () => {
+        // means the request is going through different 'phases' of the request
+        // and there's 4 in total
+        console.log(request, request.readyState)
+     })
+    
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
+    request.send()
+}
+
