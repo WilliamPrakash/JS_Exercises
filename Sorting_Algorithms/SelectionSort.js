beb02fa03bb093e5f9870a0c8643e4b2c1006e22
WilliamPrakash <willprakash@gmail.com>
2023-06-15 21:19:54 -0400

    somewhat working selection sort (rollback)

--- Sorting_Algorithms/SelectionSort.js
+++ Sorting_Algorithms/SelectionSort.js
@@ -1,14 +1,30 @@
 import swap from './helperMethods.js'
 
-// best/worst: O(N^2)
-// finds the minimum value in the data structure, places it in the
-// first position, finds the second min val, places it in the second position, etc...
+/*
+    best/worst: O(N^2)
+    Find the minimum value in the array, place it in the
+    first position, finds the second min val, places it in the second position, etc...
+*/
 
 function selectionSort(arr) {
-    
+    // find min in entire array
+    let min = arr[0]
+    let minIndex = 0
+
+    for(let j = 0; j < arr.length; j++){
+        if(min > arr[j]) {
+            min = arr[j]
+            minIndex = j
+        }
+    }
+
+    // put min at index i ??? instead of hardcoded 0
+    let temp = arr[0]
+    arr[0] = min
+    arr[minIndex] = temp
+
+    return arr
 }
 
-let unsortedArray = [3,9,5,1,0]
-let sortedArray = selectionSort(unsortedArray)
-
-console.log(sortedArray)
+let unsortedArray = [3,9,5,0,1]
+console.log(selectionSort(unsortedArray))
