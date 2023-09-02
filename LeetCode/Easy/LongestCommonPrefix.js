35b4c2381c39494a7628a3298c0cfcb06de81306
WilliamPrakash <willprakash@gmail.com>
2023-09-02 16:06:35 -0400

    finished leetCode #14

--- LeetCode/Easy/LongestCommonPrefix.js
+++ LeetCode/Easy/LongestCommonPrefix.js
@@ -1,42 +1,42 @@
 // Write a function to find the longest common prefix
 // amongst an array of strings
 
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
 
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
 
-function longestCommonPrefix(list) {
-    let len = list[0].length
-    // get shortest str in list
-    list.forEach(x => {
-        if(len > x.length) len = x.length
-    })
-
+function longestCommonPrefix(strs) {
+    
     let prefix = ''
-    let prev = 0
-    let charArray = 0
-    //let charArray = []
-    let i = 0
-    while(i < len) {
-        // put chars at same index position in array
-        list.forEach(x => {
-            charArray.push(x.charAt(i))
-        })
+    // Use first string for comparison
+    for(let i = 0; i < strs[0].length; i++) {
+        // strs[0] = flower
+        // strs[0][1] = f
+        //console.log(strs[0][i])
 
-        let check = charArray[0]
-        for(let j = 1; j < charArray.length; j++){
-            // BOOLEAN ARRAY?
-            if(check == charArray[i]) {
-
-            } else {
-
+        var equal = true;
+        // check the same index character 
+        for(let j = 1; j < strs.length; j++) {
+            if(strs[0][i] != strs[j][i]) {
+                equal = false
+                i = strs[0].length
             }
         }
-        //charCompare = []
-        i++
+
+        if(equal) {
+            prefix += strs[0][i]
+        }
+
+        //console.log(equal)
     }
+    return prefix
+    //console.log(prefix)
 }
 
 let strs = ['flower','flow','flight']
 longestCommonPrefix(strs) // output: 'fl'
 
 strs = ["dog","racecar","car"]
-//longestCommonPrefix(strs) // output: ''
+longestCommonPrefix(strs) // output: ''
