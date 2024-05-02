c65856a892dcccdf2123e8c9e9fab45fc06d1fef
WilliamPrakash <willprakash@gmail.com>
2024-05-02 07:42:05 -0400

    solved problem 26.

--- LeetCode/Easy/RemoveDuplicates.js
+++ LeetCode/Easy/RemoveDuplicates.js
@@ -1,38 +1,32 @@
-//26. Remove Duplicates from Sorted Array
 /*
-Given an integer array nums sorted in ascending order, remove the duplicates in-place 
-such that each unique element appears only once. The relative order of the elements should 
-be kept the same. Then return the number of unique elements in nums.
-    
-Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
-
-Change the array nums such that the first k elements of nums contain the unique elements in the order they
-were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
-Return k.
+Given an int array sorted in asc order,
+    remove the duplicates in-place such that each 
+    unique element appears only once. The relative
+    order of the elements should be kept the same.
+    Then return the number of unique elements in
+    nums.
 
 Input: nums = [1,1,2]
 Output: 2, nums = [1,2,_]
-Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
-It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 */
 
-let removeDuplicates = function(nums) {
+var removeDuplicates = function(nums) {
+    nums.sort() // don't assume it's sorted
+    // toSorted() doesn't mutate the original array
     
-
-    //
     for (let i = 0; i < nums.length; i++) {
-        //let next = nums[i+1]
-        if (nums[i] == nums[i+1]) {
-            
+        while (nums[i] == nums[i+1]) {
+            nums.splice(i,1)
         }
-
-
+        console.log(nums)
     }
-    console.log(nums)
-
 }
 
-//[1,1,2]
-//[0,0,1,1,1,2,2,3,3,4] -> [0,1,2,3,4]
-console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+removeDuplicates([1,1,2])
+removeDuplicates([0,0,1,1,1,2,2,3,3,4])
+
+
 
