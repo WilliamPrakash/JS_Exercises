8059c46d88415cee36699fdf8ba8ff4a4d851589
WilliamPrakash <willprakash@gmail.com>
2024-05-04 22:16:58 -0400

    problem 3. passing only 2 test cases

--- LeetCode/Medium/LongestSubstring.js
+++ LeetCode/Medium/LongestSubstring.js
@@ -10,26 +10,25 @@ Explanation: The answer is "abc", with the length of 3.
 */
 
 function lengthOfLongestSubstring(s) {
-    let temp = ""
+    let i = 1
+    let temp = s[0]
     let first = s[0]
 
-    // Idea: while iterating through all characters in the string,
-    // if the char matches the first char, 
-    for (let i = 1; i < s.length; i++) {
-        console.log(s[i])
-        //let j = i
-        while(s[i] != first) {
-            console.log("s[i]: " + s[i])
-            temp += s[i]
-            i++
-        }
-        console.log(temp)
+    /* DISCLAIMER: this logic currently relies on the 
+    beginning of a unique substring being the first char*/
+    console.log(first)
+    let x = s[i]
+    console.log(x)
+    while(s[i] !== first) {
+        console.log("s[i]: " + s[i])
+        temp += s[i]
+        i++
     }
-    console.log(temp)
-
+    //console.log(temp.length)
+    return temp.length
 }
 
-
 lengthOfLongestSubstring("abcabcbb")
-
+lengthOfLongestSubstring("bbbb")
+lengthOfLongestSubstring("pwwkew") // doesn't work
 
