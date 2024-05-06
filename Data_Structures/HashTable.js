2450accf4e980cad632111b0fd8b5f91787b83e0
WilliamPrakash <willprakash@gmail.com>
2024-05-05 22:04:01 -0400

    Reviewing hash functions

--- Data_Structures/HashTable.js
+++ Data_Structures/HashTable.js
@@ -1,4 +1,88 @@
 
+// Source: https://codepen.io/beaucarnes/pen/VbYGMb?editors=0012
+/*
+  Hash key: inputted data
+  Hash/Hash value: the index of the hash table
+*/
+
+var hash = (string, max) => {
+  let hash = 0;
+  for (let i = 0; i < string.length; i++) {
+    hash += string.charCodeAt(i);
+  }
+  return hash % max;
+};
+
+var HashTable = function() {
+  let storage = [];
+  const storageLimit = 14;
+  
+  this.print = function() {
+    console.log(storage)
+  }
+
+  this.add = function(key, value) {
+    var index = hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      storage[index] = [
+        [key, value]
+      ];
+    } else {
+      var inserted = false;
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          storage[index][i][1] = value;
+          inserted = true;
+        }
+      }
+      if (inserted === false) {
+        storage[index].push([key, value]);
+      }
+    }
+  };
+
+  this.remove = function(key) {
+    var index = hash(key, storageLimit);
+    if (storage[index].length === 1 && storage[index][0][0] === key) {
+      delete storage[index];
+    } else {
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          delete storage[index][i];
+        }
+      }
+    }
+  };
+
+  this.lookup = function(key) {
+    var index = hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      return undefined;
+    } else {
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          return storage[index][i][1];
+        }
+      }
+    }
+  };
+
+};
+
+
+console.log(hash('quincy', 10))
+
+let ht = new HashTable();
+ht.add('beau', 'person');
+ht.add('fido', 'dog');
+ht.add('rex', 'dinosour');
+ht.add('tux', 'penguin')
+console.log(ht.lookup('tux'))
+ht.print();
+
+
+
+/* Older version
 class HashTable {
   constructor() {
     this.table = new Array(127)
@@ -32,6 +116,6 @@ hashTableObj.set('Will',24)
 hashTableObj.set('Devon',26)
 hashTableObj.get('Will')
 hashTableObj.get('Devon')
-
+*/
 
 
