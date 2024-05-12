bf72852baff6dd0db6605796562e1cfa07d13c22
WilliamPrakash <willprakash@gmail.com>
2024-05-12 19:56:19 -0400

    Solved leetcode 28.

--- LeetCode/Easy/FirstOccurrence.js
+++ LeetCode/Easy/FirstOccurrence.js
@@ -16,19 +16,19 @@ var strStr = function(haystack, needle) {
     //iterate over all chars in 'haystack'
     for(let i = 0; i < haystack.length; i++) {
         //find first char in haystack that equals the
-        //first char of needle 
+        //first char of needle
+        console.log(haystack[i])
         if (needle[0] == haystack[i]) {
             let startIndex = i
-            //compare substrings?
+            //compare substrings
+            let x = haystack.substring(startIndex,i + needle.length)
             if (needle === haystack.substring(startIndex,needle.length)) {
                 return startIndex
-            }                                                                                                                                                                                                                                                   
+            }
         }
-        return -1
     }
-
+    return -1
 }
 
-
-strStr('sadbutsad','sad')
-strStr('hello','ll')
+//strStr('sadbutsad','sad')
+strStr('hello','ll') //output is currently 2
