de723d5fa1b38f26929446db38599db63e4e0995
WilliamPrakash <willprakash@gmail.com>
2024-05-18 10:32:51 -0400

    Starting problem 58.

---
+++ LeetCode/Easy/LengthOfLastWord.js
@@ -0,0 +1,26 @@
+
+/*
+58. Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+Input: s = "Hello World"
+Output: 5
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+
+Input: s = "luffy is still joyboy"
+Output: 6
+*/
+
+function lengthOfLastWord(s) {
+    //start at the end
+    //remove any 
+    for(let i = 0; i < s.length; i++) {
+        
+    }
+}
+
+console.log(lengthOfLastWord('luffy is still joyboy'))
+
