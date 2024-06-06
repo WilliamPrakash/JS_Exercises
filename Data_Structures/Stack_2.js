8ed5db1ff6495f28808a82d5409f230d221ee701
WilliamPrakash <willprakash@gmail.com>
2024-06-06 13:17:52 -0400

    Working on replicating stack + PlusOne

--- Data_Structures/Stack_2.js
+++ Data_Structures/Stack_2.js
@@ -3,23 +3,28 @@
 class stack {
     constructor(size) {
         this.stack = new Array(size)
-        // always point to empty index
-        this.pointer = size.length
+        this.pointer = this.stack.length - 1
     }
 
     push(val) {
-        if (this.pointer == 0) return
+        if (this.pointer == 0) {
+            console.log('Stack is full')
+            return
+        }
         this.stack[this.pointer] = val
         this.pointer--
     }
 
     pop() {
-        if(isEmpty) return
-        //this.stack[this.pointer] = 
+        if(this.isEmpty()) {
+            console.log('Stack is empty')
+            return
+        }
+        this.stack.pop()
     }
 
     isEmpty() {
-        if(this.stack.length == 0  ) {
+        if(this.stack.length == 0) {
             return true
         } else {
             return false
@@ -32,7 +37,16 @@ class stack {
 }
 
 
-let s1 = new stack(10)
+let s1 = new stack(5)
 s1.push(1)
 s1.push(5)
+s1.push(14)
+s1.push(27)
+s1.print()
+s1.pop()
+s1.pop()
+s1.pop()
+s1.pop()
+s1.pop() // popping: <1 empty item>
+//s1.pop()
 s1.print()
---
+++ LeetCode/Easy/PlusOne.js
@@ -0,0 +1,47 @@
+/*
+You are given a large integer represented as 
+an integer array digits, where each digits[i] 
+is the ith digit of the integer. The digits are 
+ordered from most significant to least 
+significant in left-to-right order. The large 
+integer does not contain any leading 0's.
+66. 
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+*/
+
+var plusOne = function (digits) {
+    
+    
+    
+    
+    
+    /*let strNum = ''
+    digits.forEach((x) => {
+        strNum += x
+    })
+    strNum = (Number(strNum)) + 1
+    strNum = strNum.toString()
+    digits = []
+    for (let i = 0; i < strNum.length; i++) {
+        digits[i] = Number(strNum[i])
+    }
+    return digits*/
+}
+
+//console.log(plusOne([1,2,3]))
+console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]))
+
+
+
+
+
+
+
+
+
+
