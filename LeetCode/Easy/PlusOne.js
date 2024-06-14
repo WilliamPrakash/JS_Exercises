02bbaf0cb87c90b972e08e6bf606ec25930d78a3
WilliamPrakash <willprakash@gmail.com>
2024-06-14 14:01:11 -0400

    Completed LeetCode 66 with help

--- LeetCode/Easy/PlusOne.js
+++ LeetCode/Easy/PlusOne.js
@@ -1,5 +1,5 @@
 /*
-You are given a large integer represented as 
+66. You are given a large integer represented as 
 an integer array digits, where each digits[i] 
 is the ith digit of the integer. The digits are 
 ordered from most significant to least 
@@ -15,11 +15,26 @@ Output: [4,3,2,2]
 */
 
 var plusOne = function (digits) {
+    // Source: https://leetcode.com/problems/plus-one/solutions/4983026/faster-than-70/
+    let i = digits.length - 1
+    // Find first single number that can be normally incremented (doesn't increase to 2 digits)
+    while(digits[i] === 9) {
+        digits[i] = 0
+        i--
+    }
+    // Is this only for cases like: 99999?
+    if(i < 0) {
+        var arr = [1]
+        // Array.prototype.concat() is used to merge two or more arrays.
+        // This method doesn't change the existing arrays, but instead returns a new array.
+        digits = arr.concat(digits)
+    } else {
+        digits[i]++
+    }
+    return digits
     
-    
-    
-    
-    
+    // https://www.w3schools.com/js/js_numbers.asp#:~:text=Integer%20Precision,accurate%20up%20to%2015%20digits.
+    // Not working, JS numbers are accurate up to 15 digits.
     /*let strNum = ''
     digits.forEach((x) => {
         strNum += x
@@ -35,13 +50,5 @@ var plusOne = function (digits) {
 
 //console.log(plusOne([1,2,3]))
 console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]))
-
-
-
-
-
-
-
-
-
+console.log(plusOne([9,9,9,9]))
 
