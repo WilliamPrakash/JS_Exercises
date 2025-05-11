932ce602589ccdc0505e09836323b41595067bad
WilliamPrakash <willprakash@gmail.com>
2025-05-11 08:49:14 -0400

    48. is hard

--- LeetCode/Medium/RotateImage.js
+++ LeetCode/Medium/RotateImage.js
@@ -4,55 +4,43 @@ image by 90 degrees (clockwise).
 
 You have to rotate the image in-place, which means you have to modify
 the input 2D matrix directly. DO NOT allocate another 2D matrix and
-do the rotation.
-*/
+do the rotation. */
+
+"use strict";
 
 var rotate = function(matrix) {
-    // Print formatted matrix:
-    console.log(matrix[0][0] + "  " + matrix[0][1] + "  " + matrix[0][2] + "  " + matrix[0][3]);
-    console.log(matrix[1][0] + "        " + matrix[1][3]);
-    console.log(matrix[2][0] + "       " + matrix[2][3]);
-    console.log(matrix[3][0] + " " + matrix[3][1] + " " + matrix[3][2] + " " + matrix[3][3]);
 
-    // Define variables needed for the function
+    function printCurrentMatrixShape() {
+        console.log(matrix[0][0] + "  " + matrix[0][1] + "  " + matrix[0][2] + "  " + matrix[0][3]);
+        console.log(matrix[1][0] + "        " + matrix[1][3]);
+        console.log(matrix[2][0] + "       " + matrix[2][3]);
+        console.log(matrix[3][0] + " " + matrix[3][1] + " " + matrix[3][2] + " " + matrix[3][3]);
+    }
+    printCurrentMatrixShape();
+
     let sideLength = matrix[0].length;
     let spacesToMove = sideLength - 1;
-    let temp = matrix[0][1];
+    let temp = matrix[0][0]; // Temp needs to be set before first iteration, otherwise we lose the element @ matrix[0][i] where i is 0
+    /* Do we need a second "temp" variable??? */
 
-    // Rotate the top layer once
+    /* Top layer iteration.
+    Replace from the back. Current element gets put in a temp variable.
+    By the end, 11 should be in temp.*/
     let i = 0;
     while (i + 1 < spacesToMove) {
-        //matrix[0][i + 1] = temp
-        
-        matrix[0][i + 1] = matrix[0][i];
-        
-        temp = matrix[0][i + 1];
-        i++;
+        /* Optmimize by */
+        if (i == 0) { // Take from below row on first iteration
+            matrix[0][i] = matrix[1][0];
+            i++;
+            temp = matrix[0][i];
+        } else {
+            temp = matrix[0][i + 1];
+            matrix[0][i] = matrix[0][i + 1];
+            i++;
+        }
     };
-    console.log();
+    printCurrentMatrixShape();
 
-    console.log(matrix[0]);
-    console.log(matrix[1]);
-    console.log(matrix[2]);
-    console.log(matrix[3]);
-    // Full loop of the outside
-    /*console.log(matrix[0][0]);
-    console.log(matrix[0][1]);
-    console.log(matrix[0][2]);
-    console.log(matrix[0][3]);
-    console.log(matrix[1][3]);
-    console.log(matrix[2][3]);
-    console.log(matrix[3][3]);
-    console.log(matrix[3][2]);
-    console.log(matrix[3][1]);
-    console.log(matrix[3][0]);
-    console.log(matrix[2][0]);
-    console.log(matrix[1][0]);*/
-    // Visual aid of outer loop
-    /*console.log(matrix[0][0] + "  " + matrix[0][1] + "  " + matrix[0][2] + "  " + matrix[0][3]);
-    console.log(matrix[1][0] + "        " + matrix[1][3]);
-    console.log(matrix[2][0] + "       " + matrix[2][3]);
-    console.log(matrix[3][0] + " " + matrix[3][1] + " " + matrix[3][2] + " " + matrix[3][3]);*/
     console.log();
 };
 
