14a8cca1478fd5fc192d8e361a1e59ccab1a2139
WilliamPrakash <willprakash@gmail.com>
2025-06-09 17:59:18 -0400

    Solved LeetCode 5. using expand from center approach. Need to redo using manacher's algorithm.

--- Leet_Code/Medium/LongestPalindromicSubstring.js
+++ Leet_Code/Medium/LongestPalindromicSubstring.js
@@ -2,99 +2,57 @@
 Given a string s, return the longest palindromic substring
 in s.
 
-Naive approach: 
+Expansion from center: 
     Iterate over each char. Then expand the search by one
     on both sides. If match, keep going. If not, move onto 
     he next char.
     O(n^2)
 Manacher's algorithm: */
 
-// Naive approach
+// Approach: Expansion from center
 var longestPalindrome = function(s) {
+    if (s.length <= 1) { return s }
+
     let sArray = s.split("");
-    let retPalindrome = sArray.length == 1 ? s : "";
+    let retPalindrome = "";
     let tempPalindrome = "";
     let ptrL, ptrR;
 
-    for (let i = 1; i < sArray.length; i++) { // don't start on first char, there's no left
-        // Go in each direction till a match isn't found
-        ptrL = i - 1, ptrR = i + 1;
-        tempPalindrome = sArray[i];
-        // What about even numbers???
-        while (ptrL > -1 && ptrR <= sArray.length) {
-            // check either side independently for double chars
-            if (sArray[ptrL] == sArray[i] && sArray[i] != sArray[ptrR]) {
-                tempPalindrome = sArray[ptrL] + sArray[i];
-                break;
-            }
-            if (sArray[ptrL] != sArray[i] && sArray[i] == sArray[ptrR]) {
-                tempPalindrome = sArray[i] + sArray[ptrR];
-                break;
+    /* For each index:
+    -Try to expand a palindrome of odd length (i as center)
+    -Try to expand a palindrome of even length (i and i+1 as center) */
+
+    for (let i = 0; i < sArray.length; i++) {
+        // Need to run all this twice for both odd and even length
+        for (let j = 0; j < 2; j++) { // j = 0 is odd, j = 1 is even
+            tempPalindrome = "";
+            ptrL = i;
+            ptrR = i + j;
+            // Go in each direction till a match isn't found
+            while (ptrL > -1 && ptrR <= sArray.length) {
+                if (sArray[ptrL] != sArray[ptrR]) {
+                    break;
+                }
+                tempPalindrome = ptrL == ptrR ? sArray[ptrL] : sArray[ptrL] + tempPalindrome + sArray[ptrR];
+                ptrL--;
+                ptrR++;
             }
 
-            if (sArray[ptrL] != sArray[ptrR]) {
-                break;
+            if (tempPalindrome.length > retPalindrome.length) {
+                retPalindrome = tempPalindrome;
             }
-            tempPalindrome = sArray[ptrL] + tempPalindrome + sArray[ptrR];
-            ptrL--;
-            ptrR++;
-        };
-
-        if (tempPalindrome.length > retPalindrome.length) {
-            retPalindrome = tempPalindrome;
-        };
+        }
     }
+
     return retPalindrome;
 };
 
 //let res = longestPalindrome("aacabdkacaa");
 //let res = longestPalindrome("babad"); // "bab" or "aba"
 //let res = longestPalindrome("cbbd"); // "bb"
-let res = longestPalindrome("aaaa"); // "aaaa"
+//let res = longestPalindrome("aaaa"); // "aaaa"
+let res = longestPalindrome("ccd"); // "bb"
 //let res = longestPalindrome("bb"); // "bb"
 //let res = longestPalindrome("a"); // "a"
 
 console.log("break here")
-
-
-
-/* This solution (don't even know if if fully works) times out in LeetCode
-var longestPalindrome = function(s) {
-    let sArray = s.split("");
-    let currentLongestPalindrome = "";
-
-    // i will be used to shift pointer1
-    for (let i = 0; i < sArray.length; i++) {
-        if (sArray.length == currentLongestPalindrome.length) {
-            break;
-        }
-        // j will be used to shift pointer2
-        for (let j = i + 1; j <= sArray.length; j++) {
-            if ( CheckIfPalindrome(sArray.slice(i,j)) && 
-                sArray.slice(i,j).length > currentLongestPalindrome.length) {
-                currentLongestPalindrome = sArray.slice(i,j)
-            }
-        }
-
-    }
-
-    if (currentLongestPalindrome.length <= 1) {
-        return currentLongestPalindrome.toString();
-    } else {
-        return currentLongestPalindrome.join("");
-    }
-    
-};
-
-function CheckIfPalindrome(s) {
-    let pointer1 = 0, pointer2 = s.length - 1;
-    while (s.length > 1) {
-        if (s.shift() != s.pop()) {
-            return false;
-        }
-        pointer1 += 1;
-        pointer2 -= 1;
-    }
-    return true
-};
-*/
--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
 To Do:
--Finish LeetCode problem 24. Swap Nodes in Pairs
+-Redo LeetCode 5. w/ Manacher's algorithm
+-Finish LeetCode 24. Swap Nodes in Pairs
 -Implement recursive BinarySearch
 -Rewrite BinaryTree
--Finish LeetCode problem 48. Rotate Image
--Finish LeetCode problem 88. Merge Sorted Array
+-Finish LeetCode 48. Rotate Image
+-Finish LeetCode 88. Merge Sorted Array
